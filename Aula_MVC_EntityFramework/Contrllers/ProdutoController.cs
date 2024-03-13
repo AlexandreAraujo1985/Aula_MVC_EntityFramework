fb2310c12fa1721500e33a64db4c0c33a587f55a
alexa <alexa@LAPTOP-UF0HUGSI>
2024-03-13 13:11:32 -0300

    Add project files.

---
+++ Aula_MVC_EntityFramework/Contrllers/ProdutoController.cs
@@ -0,0 +1,21 @@
+﻿using Aula_MVC_EntityFramework.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula_MVC_EntityFramework.Contrllers
+{
+	public class ProdutoController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			var produtos = new[]
+			{
+				new Produto { Id = 1, Nome = "Caneta", Preco = 2 },
+				new Produto { Id = 2, Nome = "Lápis", Preco = 1 },
+				new Produto { Id = 3, Nome = "Caderno", Preco = 10 }
+			}.ToList();
+
+			return View(produtos);
+		}
+	}
+}
---
+++ Aula_MVC_EntityFramework/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace Aula_MVC_EntityFramework.Models
+{
+	public class Produto
+	{
+		public int Id { get; set; }
+
+		public string Nome { get; set; }
+
+		public decimal Preco { get; set; }
+	}
+}
