9172ffbec87b4f1452f88b87976be51c2931558d
alexa <alexa@LAPTOP-UF0HUGSI>
2024-03-13 15:45:29 -0300

    Adicionando os itens da Aula 8 parte 2

--- Aula_MVC_EntityFramework/Contrllers/ProdutoController.cs
+++ Aula_MVC_EntityFramework/Contrllers/ProdutoController.cs
@@ -1,21 +1,115 @@
 ﻿using Aula_MVC_EntityFramework.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace Aula_MVC_EntityFramework.Contrllers
 {
 	public class ProdutoController : Controller
 	{
-		[HttpGet]
-		public IActionResult Index()
+		private List<Produto> ListarProdutos()
 		{
-			var produtos = new[]
+			return new List<Produto>
 			{
 				new Produto { Id = 1, Nome = "Caneta", Preco = 2 },
 				new Produto { Id = 2, Nome = "Lápis", Preco = 1 },
 				new Produto { Id = 3, Nome = "Caderno", Preco = 10 }
-			}.ToList();
-
-			return View(produtos);
+			};
 		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View(ListarProdutos());
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Details(int id)
+		{
+			var produto = ListarProdutos().Find(x => x.Id == id);
+
+			return View(produto);
+		}
+
+		[HttpPost]
+		public IActionResult Create(Produto produto)
+		{
+			if (ModelState.IsValid)
+			{
+				var produtos = ListarProdutos();
+
+				produto.Id = 4;
+				produtos.Add(produto);
+
+				return View("Index", produtos);
+			}
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Edit(int id)
+		{
+			var produto = ListarProdutos().Find(x => x.Id == id);
+
+			return View(produto);
+		}
+
+
+		//[HttpPost]
+		//public IActionResult Edit(TruckViewModel truckViewModel)
+		//{
+		//	try
+		//	{
+		//		if (ModelState.IsValid)
+		//		{
+		//			var truck = _mapper.Map<Truck>(truckViewModel);
+		//			_truckApplication.Update(truck);
+		//			return RedirectToAction("Index");
+		//		}
+
+		//		ViewBag.Message = "Invalid edit truck";
+		//		return View();
+		//	}
+		//	catch (System.Exception)
+		//	{
+		//		ViewBag.Message = "Error edit truck";
+		//		return View();
+		//	}
+		//}
+
+		[HttpGet]
+		public IActionResult Delete(int id)
+		{
+			var produto = ListarProdutos().Find(x => x.Id == id);
+
+			return View(produto);
+		}
+
+		//[HttpPost]
+		//public IActionResult Delete(TruckViewModel truckViewModel)
+		//{
+		//	try
+		//	{
+		//		if (ModelState.IsValid)
+		//		{
+		//			var truck = _mapper.Map<Truck>(truckViewModel);
+		//			_truckApplication.Delete(truck);
+		//			return RedirectToAction("Index");
+		//		}
+
+		//		ViewBag.Message = "Invalid delete truck";
+		//		return View();
+		//	}
+		//	catch (System.Exception)
+		//	{
+		//		ViewBag.Message = "Error delete truck";
+		//		return View();
+		//	}
+		//}
 	}
 }
--- Aula_MVC_EntityFramework/Models/Produto.cs
+++ Aula_MVC_EntityFramework/Models/Produto.cs
@@ -1,11 +1,18 @@
-﻿namespace Aula_MVC_EntityFramework.Models
+﻿using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aula_MVC_EntityFramework.Models
 {
 	public class Produto
 	{
 		public int Id { get; set; }
 
+		[Required(ErrorMessage = "Nome é requerido")]
+		[Display(Name = "Nome")]
 		public string Nome { get; set; }
 
+		[Required(ErrorMessage = "Preço é requerido")]
+		[Display(Name = "Preço")]
 		public decimal Preco { get; set; }
 	}
 }
